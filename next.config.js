5ab53d034d5fe315d0e2e15f3bda37d7e153167a
howellsy <jamie.howells@gmail.com>
2024-01-10 12:15:26 +0000

    Add cdn.breadbutter.io to CSP

--- next.config.js
+++ next.config.js
@@ -7,12 +7,12 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is;
-  style-src 'self' 'unsafe-inline';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is cdn.breadbutter.io;
+  style-src 'self' 'unsafe-inline' cdn.breadbutter.io;
   img-src * blob: data:;
   media-src *.s3.amazonaws.com;
   connect-src *;
-  font-src 'self';
+  font-src 'self' cdn.breadbutter.io;
   frame-src giscus.app
 `
 
