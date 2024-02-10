aff352e1be720540c3c51eeaacd636c09483d8d3
howellsy <jamie.howells@gmail.com>
2024-02-10 07:24:30 +0000

    Suggested fixes for first snippet

--- app/wordpress/snippet1.js
+++ app/wordpress/snippet1.js
@@ -1,25 +1,23 @@
 /* eslint-disable */
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", (event) => {
   const _first_name = 'input[name="FIELD_NAME"]';
   const _last_name = 'input[name="FIELD_NAME"]';
   const _email = 'input[name="FIELD_NAME"]';
-  const _form_button = 'FORM_ID button[type=submit]';
+  const _form_button = '#FORM_ID button[type=submit]';
 
-  let loadForm = ()=> {
-      let form_button = document.querySelector('FORM_ID button[type=submit]');
-      form_button.addEventListener('click', async function(event) {
+  const loadForm = () => {
+      const form_button = document.querySelector(_form_button);
+      form_button.addEventListener('click', async (event) => {
           await identifyUser();
       });
   };
   
-  let identifyUser = () => {
+  const identifyUser = () => {
       return new Promise((resolve) => {
-          let first_name = document.querySelector(_first_name).value;
-          let last_name = document.querySelector(_last_name).value;
-          let email = document.querySelector(_email).value;
-          BreadButter.api.identifyUser(email, first_name, last_name, () => {
-              resolve();
-          });
+          const first_name = document.querySelector(_first_name).value;
+          const last_name = document.querySelector(_last_name).value;
+          const email = document.querySelector(_email).value;
+          BreadButter.api.identifyUser(email, first_name, last_name, resolve);
       });
   };
   loadForm();
